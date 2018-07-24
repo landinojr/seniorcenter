e18a27746617fb3f305ed39d8e9bd656e77cc6d1
ArjunAlbertDev <34843324+ArjunAlbertDev@users.noreply.github.com>
2018-07-24 10:58:13 -0400

    Add files via upload

--- config/auth.js
+++ config/auth.js
@@ -3,12 +3,12 @@ module.exports = {
     'googleAuth':{
       clientID : "813025007730-g82hrpu9q8jc5lb5bmi9aim0bmi4oca4.apps.googleusercontent.com",
       clientSecret:"TaPEcgtbEA0bmRScX3RdBQEU",
-      'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+      'callbackURL'   : 'https://seniorcenternew.herokuapp.com/auth/google/callback'
     },
     'googleAuth2' : {
         'clientID': '323237114211-bvkkag3t6ddo9dcvdf7p0laoe99b6kap.apps.googleusercontent.com',
         'clientSecret'  : 'P1NM0g2n2IMbu32yrNnVFSS6',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : 'https://seniorcenternew.herokuapp.com/auth/google/callback'
     }
 
 };
